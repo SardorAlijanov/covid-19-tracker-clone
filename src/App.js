4d2ff134fdfc0dc4402b206dbc6834eaaeab6f35
SardorAlijanov <s.alijanov@studentinha.uz>
2020-09-07 07:32:45 +0500

    API for countries list connected

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+import { FormControl, Select, MenuItem } from "@material-ui/core";
 import "./App.css";
 
 function App() {
-  return <div className="app"></div>;
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((responce) => responce.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso3,
+          }));
+
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
+  return (
+    <div className="app">
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+    </div>
+  );
 }
 
 export default App;
